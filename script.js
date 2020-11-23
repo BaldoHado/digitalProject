2b3e4282cb6ddcf42b08c0ae569d4f24314c13f0
Ethan Lin <70667804+e7lin@users.noreply.github.com>
2020-11-22 17:55:16 -0800

    added trees?

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-onEvent("beginButton", "click", function( ) {
-  setScreen("gameScreen");
-});
-
-//draw background
-
 drawSky();
 drawGrass();
 drawPond();
+drawAllTrees();
+
+function drawAllTrees(){
+  for (var i = 0; i < 5; i++) {
+    moveTo(randomNumber(0,320),260);
+    turnTo(0);
+    drawTree(randomNumber(5,20), randomNumber(7, 15));
+  }
+}
 
 function drawSky(){
   penColor(rgb(165, 240, 231));
@@ -34,4 +37,24 @@ function drawPond(){
   dot(50);
   moveTo(120,360);
   dot(60);
+  penUp();
+}
+
+function drawTree(radius,length){
+  penWidth(20);
+  penRGB(117, 78, randomNumber(0,30));
+  penDown();
+  arcLeft(30,radius);
+    for(var i = 0; i < length; i++){
+    arcRight(30,radius);
+    arcLeft(28,radius);
+  }
+  penRGB(randomNumber(25,45), randomNumber(175,185), 47);
+  dot(randomNumber(40,60));
+  move(randomNumber(30,45), randomNumber(-40));
+  dot(randomNumber(20,40));
+  move(randomNumber(-40,-20), randomNumber(-30,-20));
+  dot(randomNumber(35,45));
+  penUp();
+}
 }
