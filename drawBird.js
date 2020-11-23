722d7d172011f7e090364d06853f6552f0929108
Ta Man 04 <48562217+BaldoHado@users.noreply.github.com>
2020-11-22 18:22:16 -0800

    Create drawBird.js

---
+++ drawBird.js
@@ -0,0 +1,17 @@
+penColor("green");
+penWidth(10);
+
+dot(15);
+
+turnRight(130);
+moveForward(40);
+turnTo(0);
+turnLeft(20);
+moveForward(30);
+turnTo(180);
+moveForward(23);
+penColor("brown");
+penWidth(7);
+moveForward(25);
+turnRight(90);
+moveForward(5);
