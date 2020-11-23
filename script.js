ce7bcc9de6afec5d0e78121a0392b43fc7f32b20
Ethan Lin <70667804+e7lin@users.noreply.github.com>
2020-11-22 17:02:55 -0800

    pond, sky, grass

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 onEvent("beginButton", "click", function( ) {
   setScreen("gameScreen");
 });
+
+//draw background
+
+drawSky();
+drawGrass();
+drawPond();
+
+function drawSky(){
+  penColor(rgb(165, 240, 231));
+  penWidth(300);
+  penUp();
+  moveTo(0, 110);
+  penDown();
+  moveTo(320,110);
+  penUp();
+}
+
+function drawGrass(){
+  penRGB(48, 199, 106);
+  penWidth(200);
+  moveTo(0, 360);
+  penDown();
+  moveTo(320,360);
+  penUp();
+}
+
+function drawPond(){
+  penRGB(41, 179, 179);
+  moveTo(50,360);
+  penDown;
+  dot(50);
+  moveTo(120,360);
+  dot(60);
+}
