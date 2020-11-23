a467d1f11ad0667486bbf8927185a959366057ef
Ethan Lin <70667804+e7lin@users.noreply.github.com>
2020-11-22 19:14:20 -0800

    eye, beak, leg color

--- drawBird.js
+++ drawBird.js
@@ -1,7 +1,14 @@
-penColor("green");
+penColor("orange");
+penWidth("5");
+moveForward(20);
+
+penColor("red");
 penWidth(10);
 
 dot(15);
+penColor("Black");
+dot(6);
+penColor("red");
 
 turnRight(130);
 moveForward(40);
@@ -10,7 +17,7 @@ turnLeft(20);
 moveForward(30);
 turnTo(180);
 moveForward(23);
-penColor("brown");
+penRGB(166, 138, 148);
 penWidth(7);
 moveForward(25);
 turnRight(90);
