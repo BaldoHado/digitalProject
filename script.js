7674a5eddac84d97e6a2b2b0d43ced38e8712d36
Ta Man 04 <48562217+BaldoHado@users.noreply.github.com>
2020-11-27 09:48:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,33 @@ function drawSky(){
   penUp();
 }
 
+function drawBird() {
+penColor("orange");
+penWidth("5");
+moveForward(20);
+
+penColor("red");
+penWidth(10);
+
+dot(15);
+penColor("Black");
+dot(6);
+penColor("red");
+
+turnRight(130);
+moveForward(40);
+turnTo(0);
+turnLeft(20);
+moveForward(30);
+turnTo(180);
+moveForward(23);
+penRGB(166, 138, 148);
+penWidth(3);
+moveForward(25);
+turnRight(90);
+moveForward(5);
+}
+
 function drawGrass(){
   penRGB(48, 199, 106);
   penWidth(200);
