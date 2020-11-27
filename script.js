b437bf79c1c1ede7424b0e34e92af9623d3b2e0e
Ta Man 04 <48562217+BaldoHado@users.noreply.github.com>
2020-11-27 09:57:29 -0800

    Update script.js
    final

--- script.js
+++ script.js
@@ -3,7 +3,48 @@ drawGrass();
 drawPond();
 drawAllTrees();
 drawBench();
+drawBird();
+drawBird();
 
+function drawBird() {
+penUp();
+moveTo(randomNumber(20,280), randomNumber(50,400));
+penDown();
+penColor("orange");
+penWidth("5");
+moveForward(20);
+
+var random;
+random = randomNumber(0,255);
+
+var random1;
+random1 = randomNumber(0,255);
+
+var random2;
+random2 = randomNumber(0,255);
+
+penColor(rgb(random,random1,random2));
+penWidth(10);
+
+dot(15);
+penColor("Black");
+dot(6);
+penColor(rgb(random,random1,random2));
+
+turnRight(130);
+moveForward(40);
+turnTo(0);
+turnLeft(20);
+moveForward(30);
+turnTo(180);
+moveForward(23);
+penRGB(166, 138, 148);
+penWidth(3);
+moveForward(25);
+turnRight(90);
+moveForward(5);
+turnTo(0);
+}
 function drawAllTrees(){
   for (var i = 0; i < 7; i++) {
     moveTo(randomNumber(0,320),260);
